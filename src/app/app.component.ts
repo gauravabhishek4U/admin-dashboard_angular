3256645cdc44cd7ed3c4efb01d2d8453c06d86ee
Abhishek Gaurav <abhishekgauravji@icloud.com>
2024-11-05 23:21:55 +0530

    added codes for sample admin dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ApplicationRef  } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+// Ensure Bootstrap JS is properly imported
+declare var bootstrap: any;
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavbarComponent, SidebarComponent, DashboardComponent]
 })
 export class AppComponent {
   title = 'admin-dashboard';
+  isSidebarClosed = false;
+
+  handleSidebarToggle(isClosed: boolean) {
+    this.isSidebarClosed = isClosed;
+  }
+
+  openSettingsModal() {
+    const settingsModal = new bootstrap.Modal(document.getElementById('settingsModal'), {
+      keyboard: false
+    });
+    settingsModal.show();
+  }
 }
